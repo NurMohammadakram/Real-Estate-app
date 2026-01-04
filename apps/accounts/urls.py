722319f6c19722581085490aa8e442335019f39e
NurMohammadakram <nurmohammad19k8@gmail.com>
2026-01-04 21:12:26 +0600

    user authentication added

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('user/', views.profile, name='profile'),
+    path('register/', views.user_register, name='user_register'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from .models import Profile
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def profile(request):
     return render(request, 'auth/profile.html')
+
+def user_register(request):
+    if request.method == "POST":
+        user = User.objects.create_user(
+            username=request.POST['username'],
+            email=request.POST['email'],
+            password=request.POST['password'],
+        )
+        Profile.objects.create(
+            user=user,
+            role=request.POST.get('role', 'user')
+        )
+        login(request, user)
+        return redirect('profile')
+    return render(request, 'auth/register.html')
+
+
+def user_login(request):
+    if request.method == "POST":
+        user = authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user:
+            login(request, user)
+            return redirect('profile')
+    return render(request, 'auth/login.html')
+
+def user_logout(request):
+    
+    logout(request)
+    return redirect('home')
+
+@login_required
+def change_password(request):
+    user = request.user
+    
+    if request.method == "POST":
+        old_password = request.POST['old_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+        
+        if not user.check_password(old_password):
+            messages.error(request, "Invalid Credentials")
+            return redirect('change_password')
+        if new_password != confirm_password:
+            messages.error(request, 'new password did not match confirm password')
+            return redirect('change_password')
+        
+        user.set_password(new_password)
+        user.save()
+        update_session_auth_hash(request, user)
+        messages.success(request, 'Password updated succesfully')
+        return redirect('profile')
+    return render(request, 'auth/change_password.html')
+    
